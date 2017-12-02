98ea170f57dd195afef7865fbcf8ed8aeff31ad4
Dorian Selimovic <selimovic.dorian@gmail.com>
2017-12-02 14:31:44 +0100

    ...

--- tp2/Main.cpp
+++ tp2/Main.cpp
@@ -11,22 +11,26 @@ using namespace std;
 
 int main(int, char *[])
 {
+	// variable qui prendra la valeur de chacun des noms de fichier des instances
 	char filename[64];
+	// pour chaque instance
 	for (int i = 1; i < 41; i++)
 	{
-		sprintf(filename, "data/INSTANCES/la%02d.dat", 3);
+		filename[0] = 0;
+		sprintf(filename, "data/INSTANCES/la%02d.dat", i);
 		Data data(filename);
 
 		int makespan;
 		vector<int> v = data.multistart(1000, &makespan);
 
-		cout << "makespan=" << makespan << " and ";
-		cout << "VecteurBierwirth<";
+		cout << "fichier : " << filename << endl;
+		cout << "makespan=" << makespan << endl;
+		cout << "vecteur_bierwirth< ";
 		for (int i : v)
 		{
-			cout << i << "|";
+			cout << i << " ";
 		}
-		cout << ">" << endl;
+		cout << ">" << endl << endl;
 	}
 	return 0;
 }
